ada82c1628ec68183b454b37fafbbc2d149d9a7e
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-03-09 13:14:32 +0530

    Update 60.C

--- 60.C
+++ 60.C
@@ -1,17 +1,28 @@
 #include <stdio.h>
+
 int main(void) 
+
 {
 int a=1,b=1,i,n,c;
-printf("\n enter the range to print the Fibonacci numbers\n");
+
+  printf("\n enter the range to print the Fibonacci numbers\n");
 scanf("%d",&n);
-printf("%d",a);
+
+  printf("%d",a);
 printf("\t%d",b);
-for(i=1;i<=n-2;i++)
-{
-c=a+b;
-printf("\t%d",c);
+
+  for(i=1;i<=n-2;i++)
+
+  {
+c
+ =a+b;
+
+    printf("\t%d",c);
 a=b;
+    
 b=c;
+    
 }
+  
 return 0;
 }
